b740898a1f4c29b59a93ef534df9a75d8cf1cc8b
Jason M <jasonmiracle15@gmail.com>
2023-10-02 08:59:49 +0700

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index', [
+        "pagetitle" => "My Library",
+        "maintitle" => "This is My Library"
+    ]);
+});
+
+// Route::get('/about', function () {
+//     return view('about');
+// });
+
+Route::view(
+    '/about',
+    'about',
+    [
+        "pagetitle" => "About Us",
+        "maintitle" => "About My Library"
+    ]
+);
