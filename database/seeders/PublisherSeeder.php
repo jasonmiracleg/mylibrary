a1b5665b14f260d7248a1877afcec073805f6e5b
jasonmiracleg <jasonmiracle15@gmail.com>
2023-11-29 10:09:47 +0700

    Managing Database

---
+++ database/seeders/PublisherSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PublisherSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('publishers')->insert([
+            'publisher' => 'Gramedia',
+            'city' => "Jakarta"
+        ]);
+        DB::table('publishers')->insert([
+            'publisher' => 'Kozko',
+            'city' => "Citraland"
+        ]);
+        DB::table('publishers')->insert([
+            'publisher' => 'Amazona',
+            'city' => "USA"
+        ]);
+        DB::table('publishers')->insert([
+            'publisher' => 'Wdym',
+            'city' => "Europe"
+        ]);
+    }
+}
